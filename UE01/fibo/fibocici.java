71449bef3217b7543d48d79d4562a40b709a33b6
sebi <joexample.com>
2026-03-11 15:52:10 +0100

    fibo 2

--- UE01/fibo/fibocici.java
+++ UE01/fibo/fibocici.java
@@ -1,11 +1,24 @@
 void main() {
-    IO.println(fib(6));
+    IO.println(fib_naiv(6));
 }
-long fib(int n){
+long fib_naiv(int n){
     if(n<=2){
         return 1;
     }
     else{
-       return fib(n-2)+fib(n-1);
+       return fib_naiv(n-2)+fib_naiv(n-1);
     }
 }
+
+long fib(int n){
+    return fib_rec(n, 1, 1);
+}
+long fib_rec(int n, int f1, int f2){
+    if (n<=1){
+        return f1;
+    }
+    else{
+        return fib_rec(n-1, f2, f1+f2);
+    }
+}
+
