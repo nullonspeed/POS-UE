bbf3598e626df1479b9bae9adc90f102bc5cd721
sebi <joexample.com>
2026-03-11 15:03:40 +0100

    combit

---
+++ UE01/DayZ/DayZZZZ.java
@@ -0,0 +1,13 @@
+void main(){
+    int day = 3;
+    switch (day) {
+        case 1:IO.println("Montag"); break;
+        case 2:IO.println("Dienstag");break;
+        case 3:IO.println("Mittwoch");break;
+        case 4:IO.println("Donnerstag");break;
+        case 5:IO.println("Freitag");break;
+        case 6:IO.println("Samstag");break;
+        case 7:IO.println("Sunndach");break;
+        default:IO.println("nu uh");
+    }
+}
